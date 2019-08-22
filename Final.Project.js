df422c3a283c688776670ccdd13905a4c2a62087
Roberey <rjrobertrey@gmail.com>
2019-08-22 01:40:30 -0700

    All Done!

---
+++ Final.Project.js
@@ -0,0 +1,30 @@
+var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+  x.play(); 
+} 
+
+function pauseAudio() { 
+  x.pause(); 
+}
+
+var y = document.getElementById("myAudio2");
+
+function playAudio2() {
+    y.play();
+}
+
+function pauseAudio2() {
+    y.pause();
+}
+
+var z = document.getElementById("myAudio3");
+
+function playAudio3() {
+    z.play();
+}
+
+function pauseAudio3() {
+    z.pause();
+}
+
